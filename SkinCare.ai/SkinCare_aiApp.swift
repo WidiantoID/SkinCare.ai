bd79e78371c1fc329d725c0deeacdc1e066c82fd
Aryan Singh <aryanskk216@gmail.com>
2025-09-10 12:37:39 +0530

    Initial Commit

---
+++ SkinCare.ai/SkinCare_aiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkinCare_aiApp.swift
+//  SkinCare.ai
+//
+//  Created by Aryan Signh on 10/09/25.
+//
+
+import SwiftUI
+
+@main
+struct SkinCare_aiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SkinCare.aiTests/SkinCare_aiTests.swift
@@ -0,0 +1,17 @@
+//
+//  SkinCare_aiTests.swift
+//  SkinCare.aiTests
+//
+//  Created by Aryan Signh on 10/09/25.
+//
+
+import Testing
+@testable import SkinCare_ai
+
+struct SkinCare_aiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
